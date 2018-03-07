d0d7d599c5057f161f3949bfd4dad7f17f894177
Benjamin Hsu <bhsu4@u.rochester.edu>
2018-03-07 17:01:12 -0500

    cleaning up function

--- l4.R
+++ l4.R
@@ -2,7 +2,7 @@ l4 <- function(x, b, c, d, e) {
   c+(d-c)/(1+exp(b*(log(x)-log(e))))
 }
 
-plot_l4 <- function(xs, params){
+plot_l4 <- function(xs, listdf, params){
   plot(x=xs, y=l4(xs, b=params$b[1], c=params$c[1],
                   d=params$d[1], e=params$e[1]), type="l",  
        xlab="Cycle", ylab="Fluorescence", ylim=c(min(df[2:ncol(df)]), max(df)))
@@ -10,12 +10,12 @@ plot_l4 <- function(xs, params){
     lines(x=xs, y=l4(xs, b=params$b[k], c=params$c[k],
                      d=params$d[k], e=params$e[k]),  
           col=k)
-    for(j in 2:ncol(A)){
+    for(j in 2:ncol(listdf$A)){
       for(h in 1:nrow(params)){
         points(x=xs, y=listdf[[h]][,j], cex=0.45)
       }
     }
   }
-  legend(0, y=max(df)+100000, c(LETTERS[1:nrow(params)]), 
-         col=1:nrow(params), ncol=8, lty=1, cex=0.5)
+  legend("topleft", c(LETTERS[1:nrow(params)]), 
+         col=1:nrow(params), ncol=2, lty=1, cex=0.5)
 }
--- l5.R
+++ l5.R
@@ -4,7 +4,7 @@ l5 <- function(x, b, c, d, e, f) {
   c + ( (d-c) / (1+exp(b*(log(x)-log(e))))^f )
 }
 
-plot_l5 <- function(xs, params){
+plot_l5 <- function(xs, listdf, params){
   plot(x=xs, y=l5(xs, b=params$b[1], c=params$c[1],
                   d=params$d[1], e=params$e[1], f=params$f[1]), type="l",  
        xlab="Cycle", ylab="Fluorescence", ylim=c(min(df[2:ncol(df)]), max(df)))
@@ -12,13 +12,13 @@ plot_l5 <- function(xs, params){
     lines(x=xs, y=l5(xs, b=params$b[k], c=params$c[k],
                      d=params$d[k], e=params$e[k], f=params$f[k]),  
           col=k)
-    for(j in 2:ncol(A)){
+    for(j in 2:ncol(listdf$A)){
       for(h in 1:nrow(params)){
         points(x=xs, y=listdf[[h]][,j], cex=0.45)
       }
     }
   }
-  legend(0, y=max(df)+100000, c(LETTERS[1:nrow(params)]), 
-         col=1:nrow(params), ncol=8, lty=1, cex=0.5)
+  legend("topleft", c(LETTERS[1:nrow(params)]), 
+         col=1:nrow(params), ncol=2, lty=1, cex=0.5)
 }
 
