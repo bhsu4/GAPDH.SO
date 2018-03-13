bd6688efdc78a03876407e48ed4dbdca5eb3e159
Benjamin Hsu <bhsu4@u.rochester.edu>
2018-03-12 18:05:42 -0700

    subsettable closure name changes

--- l4_model.R
+++ l4_model.R
@@ -2,20 +2,20 @@ l4_model <- function(x, b, c, d, e) {
   c+(d-c)/(1+exp(b*(log(x)-log(e))))
 }
 
-plot_l4 <- function(xs, listdf, params){
-  plot(x=xs, y=l4(xs, b=params$b[1], c=params$c[1],
-                  d=params$d[1], e=params$e[1]), type="l",  
+plot_l4 <- function(xs, listdf, par){
+  plot(x=xs, y=l4_model(xs, b=par$params$b[1], c=par$params$c[1],
+                        d=par$params$d[1], e=par$params$e[1]), type="l",  
        xlab="Cycle", ylab="Fluorescence", ylim=c(min(df[2:ncol(df)]), max(df)))
-  for(k in 2:nrow(params)){
-    lines(x=xs, y=l4(xs, b=params$b[k], c=params$c[k],
-                     d=params$d[k], e=params$e[k]),  
+  for(k in 2:length(subsets)){
+    lines(x=xs, y=l4_model(xs, b=par$params$b[k], c=par$params$c[k],
+                           d=par$params$d[k], e=par$params$e[k]),  
           col=k)
     for(j in 2:ncol(listdf$A)){
-      for(h in 1:nrow(params)){
+      for(h in 1:length(subsets)){
         points(x=xs, y=listdf[[h]][,j], cex=0.45)
       }
     }
   }
-  legend("topleft", c(LETTERS[1:nrow(params)]), 
-         col=1:nrow(params), ncol=2, lty=1, cex=0.5)
+  legend("topleft", c(LETTERS[1:length(subsets)]), 
+         col=1:length(subsets), ncol=2, lty=1, cex=0.5)
 }
--- l5_model.R
+++ l5_model.R
@@ -2,21 +2,21 @@ l5_model <- function(x, b, c, d, e, f) {
   c + ( (d-c) / (1+exp(b*(log(x)-log(e))))^f )
 }
 
-plot_l5 <- function(xs, listdf, params){
-  plot(x=xs, y=l5(xs, b=params$b[1], c=params$c[1],
-                  d=params$d[1], e=params$e[1], f=params$f[1]), type="l",  
+plot_l5 <- function(xs, listdf, par){
+  plot(x=xs, y=l5_model(xs, b=par$params$b[1], c=par$params$c[1],
+                        d=par$params$d[1], e=par$params$e[1], f=par$params$f[1]), type="l",  
        xlab="Cycle", ylab="Fluorescence", ylim=c(min(df[2:ncol(df)]), max(df)))
-  for(k in 2:nrow(params)){
-    lines(x=xs, y=l5(xs, b=params$b[k], c=params$c[k],
-                     d=params$d[k], e=params$e[k], f=params$f[k]),  
+  for(k in 2:length(subsets)){
+    lines(x=xs, y=l5_model(xs, b=par$params$b[k], c=par$params$c[k],
+                           d=par$params$d[k], e=par$params$e[k], f=par$params$f[k]),  
           col=k)
     for(j in 2:ncol(listdf$A)){
-      for(h in 1:nrow(params)){
+      for(h in 1:length(subsets)){
         points(x=xs, y=listdf[[h]][,j], cex=0.45)
       }
     }
   }
-  legend("topleft", c(LETTERS[1:nrow(params)]), 
-         col=1:nrow(params), ncol=2, lty=1, cex=0.5)
+  legend("topleft", c(LETTERS[1:length(subsets)]), 
+         col=1:length(subsets), ncol=2, lty=1, cex=0.5)
 }
 
