dffcbbf49af82058a15c8d3d5d7267fa75d51e64
Benjamin Hsu <bhsu4@u.rochester.edu>
2018-03-15 10:44:35 -0700

    eff phase w/o complete fcn

--- eff_phase.R
+++ eff_phase.R
@@ -5,71 +5,21 @@ for (i in 1:8){
     subsetsb[[i]][[k]] <- df[[i]][[k]] - par$params$c[[i]]
     }
 }
-  return(list(subsetsb, subsetsbE))
+  return(list(subsetsb))
 }
-
-subsetsb_b5 <- genparamsbase(subsets, df_b5)
-
-
-subsetsbE <- lapply(1, matrix, data= NA, nrow=39, ncol=13)
-
-subsetsbE <- subsets
-for (i in 1:8){
-  for (k in 2:13){
-    for (j in 2:40){
-      subsetsbE[[i]][[k]] <- (subsetsb_b5[[i]][[k]][j,])/(subsetsb_b5[[i]][[k]][j-1,])
-    }
-  }
-}
-
-testing[[1]][[1]] <- (subsetsb_b5[[1]][[1]][2,]/subsetsb_b5[[1]][[1]][1,])
-
-
-testing <- data.frame(subsetsb_b5)
-testing2 <- testing
-for (k in 2:40){
-testing2[k-1,] <- testing[k,]/testing[k-1,]
-}
-testing2 <- abs(testing2[-40,])
-testing2 <- testing2[!grepl("Cycle",colnames(testing2))]
-Cycles <- 1:39
-testing2 <- cbind(Cycles, testing2)                       
-colnames(testing2) <- colnames(df)
-subsetstest <- lapply(LETTERS[1:8], function(k) cbind(Cycles, testing2[,colnames(testing2) %like% k]))
-names(subsetstest) <- LETTERS[1:8]
-subsetstest$C <- subsetstest$C[,-c(1)]
-
-
-plot(x = Cycles, y = subsetstest[[1]][[2]], type="p", col = 1, ylab = "Fluorescence Efficiency")
-for (i in 3:13){
-  for (j in 1:39){
-    
-  points(x = Cycles, y = subsetstest[[1]][[i]], col = i-1)
-  points(x = Cycles, y = rowMeans(subsetstest[[1]][j,-1]), pch  = 16 )
-    
-  }
-}
-
-for (j in 1:39){
-ind2 <- 39*j
-ind1 <- ind2-38
-dftest[[i]][ind1] <- rowMeans(subsetstest[[1]][j,-1])
-}
-
 
 plot_eff <- function(xs, listdf){
-for (k in 1:length(listdf)){
-  for (i in 2:13){
-    for (j in 1:39){
-    if (i == 2){
-    plot(x = xs, y = listdf[[k]][[i]], type = "p", col = 1, xlab = "Cycle", ylab = "Fluorescence Efficiency")
+  for (k in 1:length(listdf)){
+    for (i in 2:13){
+        if (i == 2){
+          plot(x = xs, y = listdf[[k]][[i]], type = "p", col = 1, xlab = "Cycle", ylab = "Fluorescence Efficiency")
+        }
+          points(x = xs, y = listdf[[k]][[i]], col = i-1)
+        for (j in 1:39){
+          points(x = xs[j], y = rowMeans(listdf[[k]][j,-1]), pch = 16)
+        }
     }
-    points(x = xs, y = listdf[[k]][[i]], col = i-1)
-    points(x = xs[j], y = rowMeans(listdf[[k]][j,-1]), pch = 16)
+    title(main = names(listdf[k]))
+    
+    }
   }
-}
-  title(main = names(listdf[k]))
-}
-}
-
-plot_eff(Cycles, subsetstest)
--- gruntfile.R
+++ gruntfile.R
@@ -34,7 +34,38 @@ plot_subset(Cycle, df_l4, df_l5, df_b4, df_b5, subsets)
 source("GAPDH.SO/plot_together.R") 
 plot_together(Cycle, df_l4, df_l5, df_b4, df_b5, subsets)
 
-source("GAPDH.SO/eff.phase.R") 
+
+
+### subsetting with efficiencies / run fluo eff plots
+
+source("GAPDH.SO/eff_phase.R") 
 subsetsb_b5 <- genparamsbase(subsets, df_b5)
 
-library(RSTAR)
+#subsetsbE <- lapply(1, matrix, data= NA, nrow=39, ncol=13)
+
+#subsetsbE <- subsets
+#for (i in 1:8){
+#  for (k in 2:13){
+#    for (j in 2:40){
+#      subsetsbE[[i]][[k]] <- (subsetsb_b5[[i]][[k]][j,])/(subsetsb_b5[[i]][[k]][j-1,])
+#    }
+#  }
+#}
+
+#testing[[1]][[1]] <- (subsetsb_b5[[1]][[1]][2,]/subsetsb_b5[[1]][[1]][1,])
+
+testing <- data.frame(subsetsb_b5)
+testing2 <- testing
+for (k in 2:40){
+  testing2[k-1,] <- testing[k,]/testing[k-1,]
+}
+testing2 <- abs(testing2[-40,])
+testing2 <- testing2[!grepl("Cycle",colnames(testing2))]
+Cycles <- 1:39
+testing2 <- cbind(Cycles, testing2)                       
+colnames(testing2) <- colnames(df)
+subsetstest <- lapply(LETTERS[1:8], function(k) cbind(Cycles, testing2[,colnames(testing2) %like% k]))
+names(subsetstest) <- LETTERS[1:8]
+subsetstest$C <- subsetstest$C[,-c(1)]
+
+plot_eff(Cycles, subsetstest)
