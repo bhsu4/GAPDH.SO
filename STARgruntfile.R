b90223f437230042143875778668989e0d59378e
Benjamin Hsu <bhsu4@u.rochester.edu>
2018-04-22 18:20:02 -0400

    stargrunt w/o functions

--- STARgruntfile.R
+++ STARgruntfile.R
@@ -2,6 +2,7 @@
 
 #Chow test single break confirm
 source("GAPDH.SO/strucbreak.R")
+library(strucchange)
 breakA <- strbreak_chow(Cycle, subsets)
 
 plot(x=5:35, y = breakA[[1]][[1]][,1], ylab = "F-statistic", xlab = "Cycle")
@@ -17,14 +18,23 @@ for(j in 2:ncol(subsets$A)){
 } #scaled overlay of points
 
 #creating list for values to run through nrep
+ts_subsets <- ts(subsets)
+for(i in 1:8){
+  for(j in 1:13){
+    ts_subsets[[i]][,j] <- ts(subsets[[i]][,j])
+  }
+}
 names(ts_subsets$F)[8] <- "F7" #replace F.6.1
 
+source("GAPDH.SO/lag_gen.R")
+library(dynlm)
+library(Hmisc)
 fftest <- lapply(LETTERS[1:8], paste0, 1:12) #list of subset names
 fftry <- lapply(fftest, function(x) lapply(x, get_lag_formulae, n=15)) #dynlm formula in list
 ff_fitstest <- list()
 for(j in 1:8) ff_fitstest[[j]] <- lapply(fftry[[j]], function(x) lapply(x, 
-                                                                        function(f) dynlm(formula = as.formula(f), 
-                                                                                          data=ts_subsets[[j]]))) #output results of dynlm
+                                                     function(f) dynlm(formula = as.formula(f), 
+                                                                 data=ts_subsets[[j]]))) #output results of dynlm
 for(j in 1:8) ff_fitstestaic[[j]] <- sapply(ff_fitstest[[j]], 
                                             function(x) sapply(x,AIC)) #output results of dynlm
 
@@ -65,8 +75,8 @@ A1test <- log10(subsets$A$A1)
 A1test <- cbind(A1test, lag(subsets$A$A1, k=-2))
 colnames(A1test) <- c("y", "ylag2")
 A1test <- ts(A1test)
-re.a1 <- efp(y ~ ylag1, data = A1test, type = "RE")
-plot(re.a1)
+#re.a1 <- efp(y ~ ylag1, data = A1test, type = "RE")
+#plot(re.a1)
 bp.a1 <- breakpoints(y ~ ylag2, data = A1test)
 summary(bp.a1)
 plot(A1test[,"y"], ylab = expression(log[10](Fluorescence))) 
@@ -96,7 +106,7 @@ for(i in 1:8){
 lsubsets <- subsets #lsubsets different from subsets_log b/c subsets_log with ts 
 for(i in 1:8){
   for(j in 2:13){
-    lsubsets[[i]][[j-1]] <- log10(subsets[[i]][[j]])
+    lsubsets[[i]][[j]] <- log10(subsets[[i]][[j]])
   }
 }
 
@@ -106,7 +116,7 @@ ff2 <- apply(lsubsets$A[,2:13], 1, median)
 try.lstar2 <- tsDyn::lstar(fftest, m=2, d=1)
 try.lstar <- tsDyn::lstar(ff, m=2, d=1) #embedding dimension=2, delay = 1 #mean
 
-plot(x=3:40, try.lstar$fitted.values, type = "l", ylim = c(5.1, 5.75), xlim=c(1,40))
+plot(x=3:40, try.lstar$fitted.values, type = "l") #ylim = c(5.1, 5.75), xlim=c(1,40))
 for(j in 1:12){
   for(h in 1:8){
     points(x=1:40, y=subsets_log[[h]][[j]][,1], cex=0.45)
@@ -167,49 +177,82 @@ plot_resid(df_l4_log, l4resids)
 ##each replication set 
 lstarmod <- list()
 for(j in 1:8) lstarmod[[j]] <- lapply(lsubsets[[j]][,2:13], function(f) try(tsDyn::lstar(f, m=2, d=1)))
-#doesn't work???
 
-lstarmod1 <- lapply(lsubsets[[1]][,2:13], function(x) tsDyn::lstar(x, m=2, d=1))
-lstarmod2 <- lapply(lsubsets[[2]][,2:13], function(x) tsDyn::lstar(x, m=2, d=1))
-lstarmod3 <- lapply(lsubsets[[3]][,2:13], function(x) tsDyn::lstar(x, m=2, d=1))
-lstarmod4 <- lapply(lsubsets[[4]][,2:13], function(x) tsDyn::lstar(x, m=2, d=1))
-lstarmod5 <- lapply(lsubsets[[5]][,2:13], function(x) tsDyn::lstar(x, m=2, d=1))
-lstarmod6 <- lapply(lsubsets[[6]][,2:13], function(x) tsDyn::lstar(x, m=2, d=1))
-lstarmod7 <- lapply(lsubsets[[7]][,2:13], function(x) tsDyn::lstar(x, m=2, d=1))
-lstarmod8 <- lapply(lsubsets[[8]][,2:13], function(x) tsDyn::lstar(x, m=2, d=1))
+for(h in 1:8){
+  for(j in 1:12){
+        if(j-1 == 1){
+      plot(x=3:40, lstarmod[[h]][[j-1]]$fitted.values, type = "l", ylabel = expression(log[10](Fluorescence)),
+           xlabel = "Time Series (t)", ylim = c(5.1, 5.75), xlim=c(1,40))
+    }
+      lines(x=3:40, lstarmod[[h]][[j]]$fitted.values, type = "l", col = j)
+      points(x=1:40, y=lsubsets[[h]][,j+1], cex=0.45)
+    }
+} 
+legend("bottomleft", c(fftest[[1]]), col = 1:12, lty=1, ncol=4, cex=0.5)
 
-lstarmod1nolog <- lapply(subsets[[1]][,2:13], function(x) tsDyn::lstar(x, m=2, d=1))
+#fitting lstar model onto data points
+for(h in 1:8){
+  for(j in 1:12){
+    tryCatch({
+      if(j == 1){
+        plot(x=3:40, lstarmod[[h]][[j]]$fitted.values, type = "l", ylabel = expression(log[10](Fluorescence)),
+             xlabel = "Time Series (t)", ylim = c(5.1, 5.75), xlim=c(1,40))
+      }
+      lines(x=3:40, lstarmod[[h]][[j]]$fitted.values, type = "l", col = j)
+      points(x=1:40, y=lsubsets[[h]][,j+1], cex=0.45)
+    }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+  }
+legend("bottomright", c(fftest[[h]]), col = 1:12, lty=1, ncol=4, cex=0.5)
+}
+#B1 is error, so A and B captured into same plot. fix this!!
+
+
+#residual plot
+
+for(h in 1:8){
+  for(j in 1:12){
+    tryCatch({
+      if(j == 1){
+        plot(x=3:40, lstarmod[[h]][[j]]$residuals, type = "l", ylabel = expression(log[10](Fluorescence)),
+             xlabel = "Time Series (t)", xlim=c(1,40))
+      }
+      lines(x=3:40, lstarmod[[h]][[j]]$residuals, type = "l", col = j)
+    }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+  }
+  legend("bottomright", c(fftest[[h]]), col = 1:12, lty=1, ncol=4, cex=0.5)
+}
+
+#type the above into function w/output residual + plot functions
+#lstar vs l,b into one 5x1 graph 
+
+#nolog fittings below
+lstarmodnolog <- list()
+for (j in 1:8) lstarmodnolog[[j]] <- lapply(subsets[[j]][,2:13], function(f) try(tsDyn::lstar(f, m=2, d=1)))
 
 for(h in 1:8){
   for(j in 1:12){
     if(j == 1){
-      plot(x=3:40, lstarmod1[[1]]$fitted.values, type = "l", ylim = c(5.1, 5.75), xlim=c(1,40))
+      plot(x=3:40, lstarmodnolog[[h]][[j]]$fitted.values, type = "l", ylim=c(130000, 505000), 
+           xlim=c(1,40), ylabel = "Fluorescence", xlabel = "Time Series (t)")
     }
-    lines(x=3:40, lstarmod1[[j]]$fitted.values, type = "l", col = j)
+      lines(x=3:40, lstarmodnolog[[h]][[j]]$fitted.values, type = "l", col = j)
     
-    points(x=1:40, y=subsets_log[[h]][[j]][,1], cex=0.45)
+    points(x=1:40, y=subsets[[h]][[j]], cex=0.45)
   }
+legend("bottomright", c(fftest[[h]]), col = 1:12, lty=1, ncol=4, cex=0.5)
 } 
 
-for(i in 1:8){
-  if(i == 1){ #plot residuals/ note: in ln units
-    plot(x = 1:38, y = lstarmod1[[i]]$residuals, type = "l", col = 1, cex = 1.5, 
-         ylab = "Residuals", xlab = "Time Series Cycle (t)")
-  }
-  lines(x = 1:38, y = lstarmod1[[i]]$residuals, col = i, cex = 1.5)
-}
-legend("bottomright", c(names(lstarmod1)), col=1:8, ncol = 6, lty = 1, cex=0.75)
-
-
 #nolog residual plots
-for(i in 1:8){
-  if(i == 1){ #plot residuals/ note: in ln units
-    plot(x = 1:38, y = lstarmod1nolog[[i]]$residuals, type = "l", col = 1, cex = 1.5, 
-         ylab = "Residuals", xlab = "Time Series Cycle (t)")
+for(h in 1:8){
+  for(j in 1:12){
+    if(j == 1){ #plot residuals fluo
+      plot(x = 1:38, y = lstarmodnolog[[h]][[j]]$residuals, type = "l", col = 1, cex = 1.5, 
+           ylab = "Residuals", xlab = "Time Series Cycle (t)")
   }
-  lines(x = 1:38, y = lstarmod1nolog[[i]]$residuals, col = i, cex = 1.5)
+  lines(x = 1:38, y = lstarmodnolog[[h]][[j]]$residuals, col = j, cex = 1.5)
+  }
+legend("bottomright", c(fftest[[h]]), col = 1:12, lty=1, ncol=4, cex=0.5)
 }
-legend("bottomright", c(names(lstarmod1nolog)), col=1:8, ncol = 6, lty = 1, cex=0.75)
 
 #original b5
 plot_resid(df_b5, b5resids)
