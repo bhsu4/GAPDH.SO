6bf751b1cecd186c6160704613b76ce99ea3f4ee
Benjamin Hsu <bhsu4@u.rochester.edu>
2018-06-13 13:30:25 -0400

    complete sig model function

--- b4_model.R
+++ b4_model.R
@@ -2,37 +2,44 @@ b4_model <- function(x, b, c, d, e) {
   c+(d-c)/(1+exp(b*(x-e)))
 }
 
-plot_b4 <- function(xs, listdf, par){
+plot_b4 <- function(listdf, par){
+  cycunl = lapply(listdf, function(x) unlist(x$Cycle))
+  cycmax <- max(unlist(cycunl)) ; xs = 1:cycmax
   plot(x=xs, y=b4_model(xs, b=par$params$b[1], c=par$params$c[1],
-                        d=par$params$d[1], e=par$params$e[1]), type="l",  
-       xlab="Cycle", ylab="Fluorescence", ylim=c(min(df[2:ncol(df)]), max(df)))
-  for(k in 2:length(subsets)){
+                            d=par$params$d[1], e=par$params$e[1]), 
+       type="l", xlab="Cycle", ylab="Fluorescence", 
+       ylim=c(range(unlist(listdf)[(names(unlist(listdf))[!grepl("Cycle", 
+                                                                 names(unlist(listdf)))])])))
+  for(k in 2:length(listdf)){
     lines(x=xs, y=b4_model(xs, b=par$params$b[k], c=par$params$c[k],
-                           d=par$params$d[k], e=par$params$e[k]),  
-          col=k)
-    for(j in 2:ncol(listdf$A)){
-      for(h in 1:length(subsets)){
-        points(x=xs, y=listdf[[h]][,j], cex=0.45)
+                               d=par$params$d[k], e=par$params$e[k]), col=k)
+    for(j in 2:length(try[[i]])){
+      for(h in 1:length(listdf)){
+        points(x=xs, y=listdf[[h]][,j][1:cycmax], cex=0.45)
       }
     }
   }
-  legend("topleft", c(LETTERS[1:length(subsets)]), 
-         col=1:length(subsets), ncol=2, lty=1, cex=0.5)
+  legend("topleft", c(LETTERS[1:length(listdf)]), 
+         col=1:length(listdf), ncol=2, lty=1, cex=0.5)
 }
 
-subplot_b4 <- function(xs, listdf, par){
+subplot_b4 <- function(listdf, par){
+  xs = listdf$Cycle
   plot(x=xs, y=b4_model(xs, b=par$params$b[1], c=par$params$c[1],
-                        d=par$params$d[1], e=par$params$e[1]), type="l",  
-       xlab="Cycle", ylab="Fluorescence", ylim=c(min(df[2:ncol(df)]), max(df))) #xaxt = "n"
-  for(k in 2:length(listdf)){
+                            d=par$params$d[1], e=par$params$e[1]), type="l",  
+       xlab="Cycle", ylab="Fluorescence", 
+       ylim=c(range(unlist(listdf)[(names(unlist(listdf))[!grepl("Cycle", 
+                                    names(unlist(listdf)))])]))) #xaxt="n", yaxt="n"
+  for(k in 2:(length(listdf)-1)){
     lines(x=xs, y=b4_model(xs, b=par$params$b[k], c=par$params$c[k],
-                           d=par$params$d[k], e=par$params$e[k]),  
-          col=k)
-    for(j in 2:ncol(listdf)){
+                               d=par$params$d[k], e=par$params$e[k]), col=k)
+    for(j in 2:length(listdf)){
       points(x=xs, y=listdf[,j], cex=0.45)
     }
   }
-  legend("topleft", c(names(listdf)[2:13]), 
-         col=1:length(listdf), ncol=2, lty=1, cex=0.65, 
-         x.intersp=0.25, text.width=c(rep(0,6), rep(0.5,6)))
+  legend("topleft", c(names(listdf)[2:length(listdf)]), 
+         col=1:(length(listdf)-1), ncol=2, lty=1, cex=0.65)
+         #x.intersp=0.25, text.width=c(rep(0,6), rep(0.5,6))
 }
+
+
--- l4_model.R
+++ l4_model.R
@@ -2,36 +2,44 @@ l4_model <- function(x, b, c, d, e) {
   c+(d-c)/(1+exp(b*(log(x)-log(e))))
 }
 
-plot_l4 <- function(xs, listdf, par){
+plot_l4 <- function(listdf, par){
+  cycunl = lapply(listdf, function(x) unlist(x$Cycle))
+  cycmax <- max(unlist(cycunl)) ; xs = 1:cycmax
   plot(x=xs, y=l4_model(xs, b=par$params$b[1], c=par$params$c[1],
-                        d=par$params$d[1], e=par$params$e[1]), type="l",  
-       xlab="Cycle", ylab="Fluorescence", ylim=c(min(df[2:ncol(df)]), max(df)))
-  for(k in 2:length(subsets)){
-    lines(x=xs, y=l4_model(xs, b=par$params$b[k], c=par$params$c[k],
-                           d=par$params$d[k], e=par$params$e[k]),  
-          col=k)
-    for(j in 2:ncol(listdf$A)){
-      for(h in 1:length(subsets)){
-        points(x=xs, y=listdf[[h]][,j], cex=0.45)
-      }
-    }
-  }
-  legend("topleft", c(LETTERS[1:length(subsets)]), 
-         col=1:length(subsets), ncol=2, lty=1, cex=0.5)
-}
-
-subplot_l4 <- function(xs, listdf, par){
-  plot(x=xs, y=l4_model(xs, b=par$params$b[1], c=par$params$c[1],
-                        d=par$params$d[1], e=par$params$e[1]), type="l",  
-       xlab="Cycle", ylab="Fluorescence", ylim=c(min(df[2:ncol(df)]), max(df)))
+                            d=par$params$d[1], e=par$params$e[1]), 
+                        type="l", xlab="Cycle", ylab="Fluorescence", 
+       ylim=c(range(unlist(listdf)[(names(unlist(listdf))[!grepl("Cycle", 
+                                    names(unlist(listdf)))])])))
   for(k in 2:length(listdf)){
     lines(x=xs, y=l4_model(xs, b=par$params$b[k], c=par$params$c[k],
                            d=par$params$d[k], e=par$params$e[k]), col=k)
-    for(j in 2:ncol(listdf)){
+    for(j in 2:length(try[[i]])){
+      for(h in 1:length(listdf)){
+        points(x=xs, y=listdf[[h]][,j][1:cycmax], cex=0.45)
+      }
+    }
+  }
+  legend("topleft", c(LETTERS[1:length(listdf)]), 
+         col=1:length(listdf), ncol=2, lty=1, cex=0.5)
+}
+
+subplot_l4 <- function(listdf, par){
+  xs = listdf$Cycle
+  plot(x=xs, y=l4_model(xs, b=par$params$b[1], c=par$params$c[1],
+                            d=par$params$d[1], e=par$params$e[1]), type="l",  
+       xlab="Cycle", ylab="Fluorescence", 
+       ylim=c(range(unlist(listdf)[(names(unlist(listdf))[!grepl("Cycle", 
+                                    names(unlist(listdf)))])]))) #xaxt="n", yaxt="n"
+  for(k in 2:(length(listdf)-1)){
+    lines(x=xs, y=l4_model(xs, b=par$params$b[k], c=par$params$c[k],
+                               d=par$params$d[k], e=par$params$e[k]), col=k)
+    for(j in 2:length(listdf)){
       points(x=xs, y=listdf[,j], cex=0.45)
     }
   }
-  legend("topleft", c(names(listdf)[2:13]), 
-         col=1:length(listdf), ncol=2, lty=1, cex=0.65,
-         x.intersp=0.25, text.width=c(rep(0,6), rep(0.5,6)))
+  legend("topleft", c(names(listdf)[2:length(listdf)]), 
+         col=1:(length(listdf)-1), ncol=2, lty=1, cex=0.65)
+         #x.intersp=0.25, text.width=c(rep(0,6), rep(0.5,6))
 }
+
+
