942345359f4db62f36cfc621281a178fb1bdbfba
Benjamin Hsu <bhsu4@u.rochester.edu>
2018-10-03 21:43:49 -0400

    general notation for abs quant

--- branch_model.R
+++ branch_model.R
@@ -36,11 +36,11 @@ sigma = pkn/mkn * ykn
 
 nexp = 26
 #Weighted cconditional least squares estimator ???
-pkn.reps <- matrix(rep(NA, 4*8), nrow = 8)
-ykn.reps <- matrix(rep(NA, 4*8), nrow = 8)
-pkn.each <- rep(NA, 4) ; ykn.each <- rep(NA, 4)
+pkn.reps <- matrix(rep(NA, 12*8), nrow = 8)
+ykn.reps <- matrix(rep(NA, 12*8), nrow = 8)
+pkn.each <- rep(NA, 12) ; ykn.each <- rep(NA, 12)
 for(i in 1:8){
-  for(j in 2:5){
+  for(j in 2:13){
     texp = b4CT[[i]]
     pkn.num = sum(subsets[[i]][,j][1:nexp]) - subsets[[i]][,j][texp] - sum(subsets[[i]][,j][1:(nexp-1)])
     pkn.den = sum(subsets[[i]][,j][texp:nexp])
@@ -49,19 +49,67 @@ for(i in 1:8){
     ykn.each[j-1] <- sum(subsets[[i]][,j])
   }
   #fill in matrix
-  pkn.reps[i,1:4] <- pkn.each
-  ykn.reps[i,1:4] <- ykn.each
+  pkn.reps[i,1:12] <- pkn.each
+  ykn.reps[i,1:12] <- ykn.each
 }
-mkn.reps <- (1+pkn.reps)^(4+1)
+mkn.reps <- (1+pkn.reps)^(12+1)
 mkn <- rowSums(mkn.reps)
 ykn <- rowSums(ykn.reps)
 pkn <- rowSums(pkn.reps)
 
 sigma = pkn/mkn*ykn
 
+abs_quant <- function(df_mod, subsets, dil = "reps"){
+ ##key lengths defined
+  replength = length(subsets[[1]])-1 
+  sublength = length(subsets)
+ ##finding start and end of exp phase 
+  res1 <- list()
+  for(i in 1:sublength){
+    res1[[i]] <- getPar(df_mod$fits[[i]], type = "curve", cp = "cpD2", eff = "sliwin")
+    res1[[i]] <- res1[[i]][-2,]
+  }
+  CTval <- ceiling(do.call(cbind, res1))
+###Proposition #1: m_{an}
+ ##empty matrices for replication results
+  pkn.reps <- matrix(rep(NA, replength*sublength), nrow = sublength)
+  ykn.reps <- matrix(rep(NA, replength*sublength), nrow = sublength)
+  pkn.each <- rep(NA, replength) ; ykn.each <- rep(NA, replength)
+ ##finding replication values
+  #pkn = estimator of efficiency for kth reaction in exp phase
+  #mkn = 1 + pkn
+  #ykn = fluorescence sum in exp phase
+  for(i in 1:sublength){
+    for(j in 2:(replength+1)){
+      texp = CTval[[i]]
+      pkn.num = sum(subsets[[i]][,j][1:nexp]) - subsets[[i]][,j][texp] - sum(subsets[[i]][,j][1:(nexp-1)])
+      pkn.den = sum(subsets[[i]][,j][texp:nexp])
+      #empty frames
+      pkn.each[j-1] <- pkn.num/pkn.den
+      ykn.each[j-1] <- sum(subsets[[i]][,j])
+    }
+    #fill in matrix
+    pkn.reps[i,1:replength] <- pkn.each
+    ykn.reps[i,1:replength] <- ykn.each
+  }
+ ##accumulated amounts inside Sigma
+  mkn.reps <- (1+pkn.reps)^(replength+1)
+  mkn <- rowSums(mkn.reps)
+  ykn <- rowSums(ykn.reps)
+  pkn <- rowSums(pkn.reps)
+  sigma = pkn*ykn/mkn
+ ##constants outside Sigma
+  if(dil = "reps"){
+    denom = 1/replength
+  }
+  else{
+    d1n = sum(dil) #dil is a vector b/c D1(n) = 1/r(n) * Sigma(k to r(n)) of dk
+    denom = (1/(replength*dk1))
+  }
+  #return(list(m = mkn, y = ykn, p = pkn))
+}
 
-
-
+abs_quant(df_b4, subsets = subsets)
 
 
   
