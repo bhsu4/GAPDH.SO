3b25e605e21b4d9f811c2c08d020fc8243a573ca
Benjamin Hsu <bhsu4@u.rochester.edu>
2019-02-05 23:55:45 -0500

    first row of qc matching miRcomp

--- visassessgruntfile.R
+++ visassessgruntfile.R
@@ -4,6 +4,7 @@ getfiles <- dir(path = "C:/Users/Benjamin Hsu/Desktop/Independent Study/GAPDH.SO
                 pattern =  "^targ_")
 files <- getfiles
 
+#matrix with qc
 res_b5 <- res_qc(getfiles, orgdata = miRcompData2, est = b5, b5dat2)
 res_l5 <- res_qc(getfiles, orgdata = miRcompData2, est = l5, l5dat2)
 res_b4 <- res_qc(getfiles, orgdata = miRcompData2, est = b4, b4dat2)
@@ -21,5 +22,30 @@ getmat <- dir(path = "C:/Users/Benjamin Hsu/Desktop/Independent Study/GAPDH.SO/t
 
 load(file = getmat[[1]]) ; load(file = getmat[[2]]) ; load(file = getmat[[3]]) ; load(file = getmat[[4]])
 
+#creating same structure as qpcRb5
+qpcRb5$ct
 
 
+ind2 <- length(unique(orgdata$SampleID))*k  ; ind1 <- ind2-(length(unique(orgdata$SampleID))-1)
+
+
+res_b4[1:40,]$TargetName.x
+res_b4[1:40,]$ct
+res_b4[1:40,]$Rsq
+
+res_b4[1:40,]$SampleID.x
+
+t(res_b4[1:40, c("SampleID.x", "ct")])
+cbind(data.frame)
+  
+cbind(data.frame(TargetName = unique(res_b4[1:40,]$TargetName.x)), t(res_b4[1:40, c("ct")]))
+
+#mixedsort the columns, then cbind with colnames = vector of sampleid
+
+water <- res_b4[1:40, c("SampleID.x", "ct")]
+sampids <- res_b4[1:40, c("SampleID.x")]
+river <- order(match(sampids, paste0(hello, "_", 1:4)))
+res_test <- t(water[river,]$ct) 
+
+res_test2 <- cbind(data.frame(TargetName = unique(res_b4[1:40,]$TargetName.x)), t(as.vector(res_test)))
+colnames(res_test2) <- c("TargetName", as.vector(sampids[river]))
