33807dd48f601a99e20556961e8e51dec83b8f09
Benjamin Hsu <bhsu4@u.rochester.edu>
2019-02-23 22:23:45 -0500

    sdm and threshold ct for lstar visuals

--- visassessgruntfile.R
+++ visassessgruntfile.R
@@ -40,26 +40,27 @@ boxes <- precision(res_qpcrb4, qcThreshold1=0.95, statistic="sd")
 #LSTAR ct, qc scores
 setwd("C:/Users/Benjamin Hsu/Desktop/Independent Study/GAPDH.SO/targetsmcont")
 load(file = "LSTAR_lag2.Rda") ; lstar_dat <- tstlstarmat3
-res_qpcrlstar <- gen_ctqc(lstar_dat)
+res_qpcrlstar <- gen_ctqc(lstar_dat) #SDM
+res_qpcrlstarth <- gen_ctqc(lstar_dat) #threshold method
 
 #quality assessment
-qualityAssessment(res_qpcrb4, res_qpcrlstar, label1 = "qpcR R-squared (b4)", label2 = "LSTAR R-squared")
-qualityAssessment(res_qpcrlstar, plotType = "boxplot", na.rm=TRUE, label1 = "R-squared")
+qualityAssessment(res_qpcrb4, res_qpcrlstarth, label1 = "qpcR R-squared (b4)", label2 = "LSTAR R-squared")
+qualityAssessment(res_qpcrlstarth, plotType = "boxplot", na.rm=TRUE, label1 = "R-squared")
 par(mfrow = c(2,1))
-qualityAssessment(res_qpcrb4, res_qpcrlstar, plotType="boxplot", na.rm=TRUE, label1="R-squared (b4)", label2 = "R-squared (LSTAR)")
+qualityAssessment(res_qpcrb4, res_qpcrlstarth, plotType="boxplot", na.rm=TRUE, label1="R-squared (b4)", label2 = "R-squared (LSTAR)")
 #complete features
-completeFeatures(res_qpcrlstar, qcThreshold1 = 0.95, label1 = "R-sq")
+completeFeatures(res_qpcrlstarth, qcThreshold1 = 0.95, label1 = "R-sq")
 #limit detection
-boxes <- limitOfDetection(res_qpcrlstar, qcThreshold=0.95, plotType="boxplot")
-lods <- limitOfDetection(res_qpcrlstar, qcThreshold=0.95, plotType="scatter")
-lods <- limitOfDetection(res_qpcrlstar, qcThreshold=0.95, plotType="MAplot")
+boxes <- limitOfDetection(res_qpcrlstarth, qcThreshold=0.95, plotType="boxplot")
+lods <- limitOfDetection(res_qpcrlstarth, qcThreshold=0.95, plotType="scatter")
+lods <- limitOfDetection(res_qpcrlstarth, qcThreshold=0.95, plotType="MAplot")
 #titration response
 titrationResponse(res_qpcrlstar, qcThreshold1=0.99)
-titrationResponse(res_qpcrb4, qcThreshold1=0.99, object2=res_qpcrlstar, qcThreshold2=0.99, label1="b4", label2="lstar")
+titrationResponse(res_qpcrb4, qcThreshold1=0.99, object2=res_qpcrlstarth, qcThreshold2=0.99, label1="b4", label2="lstar")
 #accuracy
 accuracy(res_qpcrlstar, qcThreshold1 = 0.9)
-boxes <- precision(res_qpcrlstar, qcThreshold1=0.95, statistic="sd")
-accuracy(res_qpcrb4, qcThreshold1=0.99, object2=res_qpcrlstar, qcThreshold2=0.99, label1="b4", label2="lstar")
+boxes <- precision(res_qpcrlstarth, qcThreshold1=0.95, statistic="sd")
+accuracy(res_qpcrb4, qcThreshold1=0.99, object2=res_qpcrlstarth, qcThreshold2=0.99, label1="b4", label2="lstar")
 
 
 ###test out other ct methods
@@ -78,3 +79,30 @@ for(i in 1:sublength){ #running LSTAR model
 }
 lstarres[[1]][[1]]$coefficients
 lstarres[[1]][[1]]$model
+
+
+
+
+coef <- lstarres[[1]][[2]]$coefficients
+
+((coef["const.L"]-coef["const.H"])+ (coef["phiL.1"]-coef["phiH.1"])* Xt_1 +
+  (coef["phiL.2"]-coef["phiH.2"])* Xt_2 )*(1/(1+exp(coef["gamma"]*(Xt_1-coef["th"])))) +
+coef["const.H"] + coef["phiH.1"]*Xt_1 + coef["phiH.2"]*Xt_2
+
+Xt_1 = subs[[1]][,3][2:39]
+Xt_2 = subs[[1]][,3][1:38]
+
+((coef["const.L"]-coef["const.H"])+ (coef["phiL.1"]-coef["phiH.1"])* Xt_1 +
+  (coef["phiL.2"]-coef["phiH.2"])* Xt_2 )*(1/(1+exp(coef["gamma"]*(Xt_1-coef["th"])))) +
+coef["const.H"] + coef["phiH.1"]*Xt_1 + coef["phiH.2"]*Xt_2
+
+((coef["const.L"] + coef["phiL.1"]*Xt_1 + coef["phiL.2"]*Xt_2))*(1/(1+exp(coef["gamma"]*(Xt_1-coef["th"])))) +
+  ((coef["const.H"] + coef["phiH.1"]*Xt_1 + coef["phiH.2"]*Xt_2))*(1-1/(1+exp(coef["gamma"]*(Xt_1-coef["th"]))))
+
+
+
+
+coef["gamma"]
+coef["th"]
+
+
